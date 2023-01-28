7cc9e12b3cdc0dcff527164a29d89c6c8e7436db
Kevin98 <kevinjespers98@gmail.com>
2023-01-28 11:11:55 +0100

    selectie oefeningen af

--- WinFormsApp1/WinFormsApp1/Selectie.vb
+++ WinFormsApp1/WinFormsApp1/Selectie.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Diagnostics.Eventing.Reader
+﻿Imports System.Configuration
+Imports System.Diagnostics.Eventing.Reader
 
 Public Class Selectie
     Dim nummer As Integer
@@ -195,38 +196,170 @@ Public Class Selectie
     End Sub
 
     Private Sub oefening7()
+        Dim jaar As Integer = InputBox("geef een jaar in")
+
+        If (jaar Mod 4 = 0) And Not (jaar Mod 100 = 0) Then
+            MsgBox("dit is een schrikkeljaar")
+        ElseIf (jaar Mod 400 = 0) Then
+            MsgBox("dit is een schrikkeljaar")
+        Else
+            MsgBox("dit is geen schrikkeljaar")
+
+
+        End If
+
 
     End Sub
 
     Private Sub oefening8()
+        Dim getal As Integer = InputBox("geef een getal")
+        Dim verschil As Integer = getal - 13
+        Dim absoluut As Integer = Math.Abs(verschil)
+
+        If absoluut > 13 Then
+            absoluut *= 2
+        Else
+            MsgBox("geen verdubbeling")
+        End If
+
+        MsgBox(absoluut)
 
     End Sub
 
     Private Sub oefening9()
+        Dim getal_1 As Double = InputBox("getal 1")
+        Dim getal_2 As Double = InputBox("getal 2")
+        Dim som As Double = getal_1 + getal_2
+
+        If getal_1 = getal_2 Then
+            som *= 3
+            MsgBox("maal 3")
+        End If
+
+        MsgBox(som)
 
     End Sub
 
     Private Sub oefening10()
+        Dim getal_1 As Double = InputBox("1e getal")
+        Dim getal_2 As Double = InputBox("2e getal")
+        Dim getal_3 As Double = InputBox("3e getal")
+        Dim getal_4 As Double = InputBox("4e getal")
+        Dim even As Boolean = False
+
+        If getal_3 Mod 2 = 0 Then
+            even = True
+            MsgBox("getal 3 is even")
+        End If
+
+
+        If getal_1 < 0 Or getal_2 < 0 Or even = False Or getal_3 < (getal_2 + getal_1) Or getal_4 < (getal_3 + getal_2) Then
+            MsgBox("foute waarde")
+        Else
+            MsgBox("juiste waarde")
+
+        End If
 
     End Sub
 
     Private Sub oefening11()
+        Dim getal(4) As Double
+        Dim v As Integer
+        Dim oneven As Integer = 0
+
+
+        For v = 0 To 4
+            getal(v) = InputBox("geef getal " & v + 1 & " in")
+            MsgBox(getal(v) Mod 2)
+            If (getal(v) Mod 2 = 0) Then
+                MsgBox("even")
+            Else
+                oneven += getal(v)
+                MsgBox("oneven")
+            End If
+        Next
+
+        MsgBox(oneven)
 
     End Sub
 
     Private Sub oefening12()
+        Dim getal_1 As Integer = InputBox("1e getal")
+        Dim getal_2 As Integer = InputBox("2e getal")
 
+        If getal_1 Mod getal_2 = 0 Then
+            MsgBox(getal_1 & " is een veelvoud van " & getal_2)
+        ElseIf getal_2 Mod getal_1 = 0 Then
+            MsgBox(getal_2 & " is een veelvoud van " & getal_1)
+        Else
+            MsgBox("de getallen zijn geen veelvouden van elkaar")
+        End If
     End Sub
 
     Private Sub oefening13()
+        Dim v As Integer = InputBox("geef een getal van 1 tot 12")
+        Dim maand() As String = {"januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "october", "november", "december"}
+
+        MsgBox(maand(v - 1))
 
     End Sub
 
     Private Sub oefening14()
+        Dim v As Integer
+        Dim nummer(4) As Integer
+        Dim negatief As Integer = 0
+        Dim positief As Integer = 0
+        Dim somN As Integer = 0
+        Dim somP As Integer = 0
+
+        For v = 0 To 4
+            nummer(v) = InputBox("geef nummer " & v + 1 & " in")
+            If nummer(v) < 0 Then
+                negatief += 1
+                somN += nummer(v)
+            ElseIf nummer(v) = 0 Then
+                MsgBox("nulwaarde")
+            Else
+                positief += 1
+                somP += nummer(v)
+            End If
+        Next
+
+        MsgBox("er zijn " & positief & " positieve waarden" & vbCrLf & "er zijn " & negatief & " negatieve waarden")
+        MsgBox("de som van de positieve waarden is " & somP & vbCrLf & "de som van de negatieve waarden is " & somN)
+
 
     End Sub
 
     Private Sub oefening15()
+        Dim v As Integer = InputBox("kies 1 voor kg naar pond " & vbCrLf & "kies 2 voor pond naar kg" & vbCrLf & "kies 3 voor cm naar inch" & vbCrLf & "kies 4 voor inch naar cm")
+        Dim input As Double = 0
+        Dim output As Double = 0
+
+        If v = 1 Then
+            input = InputBox("kilogram naar pond")
+            output = input * 2.20462262
+            MsgBox(output & " pond")
+        End If
+
+        If v = 2 Then
+            input = InputBox("pond naar kilogram")
+            output = input / 2.20462262
+            MsgBox(output & " kg")
+        End If
+
+        If v = 3 Then
+            input = InputBox("cm naar inch")
+            output = input / 2.54
+            MsgBox(output & " inch")
+        End If
+
+        If v = 4 Then
+            input = InputBox("inch naar cm")
+            output = input * 2.54
+            MsgBox(output & " cm")
+        End If
+
 
     End Sub
 
